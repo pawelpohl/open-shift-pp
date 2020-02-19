04cb3cf3ce71fa97b1f872d2c0625430c55c9758
pawelpohl <61056276+pawelpohl@users.noreply.github.com>
2020-02-19 13:49:06 +0100

    sdded TestLab

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World by Pawel Pohl!"
+    return "Hello World by Pawel Pohl and the TestLab Team"
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)
